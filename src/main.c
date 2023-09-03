4994b52cbccca0b8ef63e26f92a609c6e78494ad
jonathan-d-zhang <69145546+jonathan-d-zhang@users.noreply.github.com>
2023-09-02 20:51:19 -0400

    feat: Switch to push-pull

--- src/main.c
+++ src/main.c
@@ -9,28 +9,24 @@
 #include "hll.h"
 
 int main() {
-    char *filter = getenv("SUB_FILTER");
+    //char *filter = getenv("SUB_FILTER");
     char *vent_address = getenv("VENT_ADDRESS");
     char *sink_address = getenv("SINK_ADDRESS");
 
-    printf("Starting with filter=%s and proxy_address=%s\n", filter, vent_address);
+    printf("Starting with proxy_address=%s\n", vent_address);
 
-    if (NULL == filter)
-    {
-        filter = "";
-    }
-    zsock_t *sub = zsock_new_sub(vent_address, filter);
+    zsock_t *pull = zsock_new_pull(vent_address);
     zsock_t *push = zsock_new_push(sink_address);
 
     char *string;
 
     while (1) {
-        string = zstr_recv(sub);
+        string = zstr_recv(pull);
         //printf("%s\n", string);
         zstr_send(push, string);
     }
 
     zstr_free(&string);
-    zsock_destroy(&sub);
+    zsock_destroy(&pull);
     return 0;
 }
